ed32b4bb43cbf999f2a4c062c9d188d21199acd8
juan01-ctrl <juanierace2@gmail.com>
2021-01-13 01:56:32 -0300

    landing 0

---
+++ dev/js/scripts.js
@@ -0,0 +1,73 @@
+//  let firstScroll = window.pageYOffset;
+
+
+//  window.addEventListener('scroll', ()=>{
+//      const actualScroll = window.pageYOffset;
+//       const header = document.getElementById('header')
+     
+//       if(firstScroll >= actualScroll){
+//           header.style.top = '0';
+//         }else{
+//             header.style.top = '-100px';
+//         }
+//         if(actualScroll < 730){
+//           header.style.top = '0';
+//         }
+//         firstScroll = actualScroll;
+//  })
+const menuBtn = document.getElementById('menu-btn')
+
+
+menuBtn.addEventListener('click', (e)=>{
+  const menu = document.getElementById('menu')
+
+  menuBtn.firstElementChild.classList.toggle('none')
+  menuBtn.lastElementChild.classList.toggle('none')
+  menu.classList.toggle('active')
+
+})
+
+document.addEventListener('click',e=>{
+    if(!e.target.matches('.menu-item')) return false;
+        menuBtn.firstElementChild.classList.remove('none')
+        menuBtn.lastElementChild.classList.add('none')
+        menu.classList.remove('active')
+   
+})
+
+
+//modal form
+
+
+  const $form = document.querySelector(".contact-form"),
+    $loader = document.querySelector(".contact-form-loader"),
+    $response = document.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/juanierace74@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
